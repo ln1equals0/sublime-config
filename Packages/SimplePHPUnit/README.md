b59df9a6edc258a0e61c0d07d5d3d2a81eec33b4
= <=>
2016-06-24 19:17:53 +0200

    my sublime text 3 config file

---
+++ Packages/SimplePHPUnit/README.md
@@ -0,0 +1,42 @@
+Simple PHPUnit commands
+===============
+
+This plugin allows you the run the PHPUnit tests using the Sublime Text interface, without having to open and use the command line.
+
+### Available commands:
+
+- `PHPUnit: Run`
+- `PHPUnit: Run with params`
+
+### Coloring output:
+
+![Coloring output](https://raw.github.com/m0nah/SimplePHPUnit-for-Sublime-Text/master/Screen%20Shot.png)
+
+### Installation:
+Use Package Controller or create a the directory `SimplePHPUnit` in your Sublime Text Packages directory, and you're ready to go.
+
+### Usage:
+Press Cmd + Shift + P for the dropdown command list, search for `PHPUnit: `, and pick your command. Also you can use `Tools/PHPUnit...` menu item
+
+### Keybinding:
+
+You can use command `simple_php_unit` for your keybinding.
+
+Example:
+
+```json
+{ "keys": ["super+ctrl+alt+t"], "command": "simple_php_unit" }
+```
+
+### Notes:
+- PHPUnit config file needs to been in the root folder of your structure in the sidebar.
+- You need insert in Sublime Text user settings `"show_panel_on_build": true` or use `Tools/Build Results/Show Build Results` menu item for view results.
+
+### Donate:
+If you liked this plugin, you can donate to support it!
+
+[![Paypal donate](https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=SZ6YWJUGFM9J8)
+
+Give some feedback.
+
+Thanks.
---
+++ Packages/SimplePHPUnit/SimplePHPUnit.py
@@ -0,0 +1,74 @@
+import os
+import sys
+import shlex
+import subprocess
+import sublime
+import sublime_plugin
+
+if sys.version_info < (3, 3):
+    raise RuntimeError('SimplePHPUnit works with Sublime Text 3 only')
+
+SPU_THEME = 'Packages/SimplePHPUnit/SimplePHPUnit.hidden-tmTheme'
+SPU_SYNTAX = 'Packages/SimplePHPUnit/SimplePHPUnit.hidden-tmLanguage'
+
+class ShowInPanel:
+    def __init__(self, window):
+        self.window = window
+
+    def display_results(self):
+        self.panel = self.window.get_output_panel("exec")
+        self.window.run_command("show_panel", {"panel": "output.exec"})
+
+        self.panel.settings().set("color_scheme", SPU_THEME)
+        self.panel.set_syntax_file(SPU_SYNTAX)
+
+class SimplePhpUnitCommand(sublime_plugin.WindowCommand):
+    def __init__(self, *args, **kwargs):
+        super(SimplePhpUnitCommand, self).__init__(*args, **kwargs)
+        settings = sublime.load_settings('SimplePHPUnit.sublime-settings')
+        self.phpunit_path = settings.get('phpunit_path')
+
+    def run(self, *args, **kwargs):
+        try:
+            # The first folder needs to be the Laravel Project
+            self.PROJECT_PATH = self.window.folders()[0]
+            if os.path.isfile("%s" % os.path.join(self.PROJECT_PATH, 'phpunit.xml')) or os.path.isfile("%s" % os.path.join(self.PROJECT_PATH, 'phpunit.xml.dist')):
+                self.params = kwargs.get('params', False)
+                self.args = [self.phpunit_path, '--stderr']
+                if self.params is True:
+                    self.window.show_input_panel('Params:', '', self.on_params, None, None)
+                else:
+                    self.on_done()
+            else:
+                sublime.status_message("phpunit.xml or phpunit.xml.dist not found")
+        except IndexError:
+            sublime.status_message("Please open a project with PHPUnit")
+
+    def on_params(self, command):
+        self.command = command
+        self.args.extend(shlex.split(str(self.command)))
+        self.on_done()
+
+    def on_done(self):
+        if os.name != 'posix':
+            self.args = subprocess.list2cmdline(self.args)
+        try:
+            self.run_shell_command(self.args, self.PROJECT_PATH)
+        except IOError:
+            sublime.status_message('IOError - command aborted')
+
+    def run_shell_command(self, command, working_dir):
+            self.window.run_command("exec", {
+                "cmd": command,
+                "shell": os.name == 'nt',
+                "working_dir": working_dir
+            })
+            self.display_results()
+            return True
+
+    def display_results(self):
+        display = ShowInPanel(self.window)
+        display.display_results()
+
+    def window(self):
+        return self.view.window()
